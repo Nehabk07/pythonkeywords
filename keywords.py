72c47d962ae05db90618e4f566a434617f2cb45b
Nehabk07 <kadamneha515@gmail.com>
2020-03-07 17:53:15 +0530

    This is for Python Keywords

---
+++ keywords.py
@@ -0,0 +1,10 @@
+import keyword
+
+# Below line shows the list of Keywords
+print(keyword.kwlist) # This will show total 35 keywords
+# ['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del',
+# 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal',
+# 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+
+print(5 > 3)
+print(True or False)
